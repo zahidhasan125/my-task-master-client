673ac14102b8cca8fcc636e836c7b416e0f5942d
zahidhasan125 <zahidhasan125@gmail.com>
2022-12-30 20:26:15 +0600

    dark theme and dark bg colur applied

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { router } from './Routes/Routes';
 
 function App() {
   return (
-    <div className="w-full mx-auto bg-[#eb5e28] min-h-screen">
+    <div className="w-full mx-auto bg-gradient-to-tr from-indigo-900 via-sky-600  to-sky-900 min-h-screen dark:bg-gradient-to-tr dark:from-gray-900 dark:via-zinc-700 dark:to-black">
       <RouterProvider router={router}>
       </RouterProvider>
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,10 +3,37 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
+    const userTheme = localStorage.getItem("theme")
+    const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    const [isDark, setIsDark] = useState(userTheme || 'light');
+
+    const themeCheck = () => {
+        if (userTheme === "dark" || (!userTheme && systemTheme)) {
+            document.documentElement.classList.add("dark");
+            return;
+        }
+    }
+
+    const themeSwitch = () => {
+        if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
+            return;
+        }
+        document.documentElement.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+    }
+
+    const handleDarkMode = () => {
+        setIsDark(!isDark);
+        themeSwitch();
+    }
+    themeCheck();
     return (
-        <div className='flex w-full md:w-[95%] max-w-7xl mx-auto bg-[#457b9d] h-16 px-4 mb-6 text-white items-center justify-between rounded-xl '>
-            <h1 className='text-2xl font-bold'>Task-Master</h1>
-            <label tabIndex={0} className="btn btn-ghost lg:hidden relative" onClick={()=>setShowMenu(!showMenu)}>
+        <div className='flex w-full md:w-[95%] max-w-7xl mx-auto bg-[#457b9d] dark:bg-stone-900 h-16 px-4 mb-6 text-white items-center justify-between rounded-xl '>
+            <h1 className='text-3xl font-extrabold uppercase border-2 border-double rounded-md'>Task-Master</h1>
+            <label tabIndex={0} className="btn btn-ghost lg:hidden relative" onClick={() => setShowMenu(!showMenu)}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 {/* Mobile menu */}
                 <ul className={`absolute w-40 z-10 right-0 ${!showMenu && 'hidden'}`}>
@@ -20,12 +47,18 @@ const Navbar = () => {
                 <ul className='flex ml-4 gap-4'>
                     <Link to="/mytasks"><li className='bg-[#1d3557] px-2 py-1 rounded-lg font-bold cursor-pointer'>My Task</li></Link>
                     <Link to="/completed"><li className='bg-[#1d3557] px-2 py-1 rounded-lg font-bold cursor-pointer'>Completed Task</li></Link>
+
                 </ul>
             </div>
-            <div className='hidden md:block'>
-                <ul>
+            <div className='md:flex hidden'>
+                <ul className='md:mr-4'>
                     <Link to="/"><li className='bg-[#1d3557] px-2 py-1 rounded-lg font-bold cursor-pointer'>+ Add Task</li></Link>
                 </ul>
+                <label className="inline-flex relative items-center cursor-pointer">
+                    <input type="checkbox" onClick={handleDarkMode} value="" className="sr-only peer" defaultChecked={isDark === 'dark' ? true : false}/>
+                    <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute dark:border-gray-600 peer-checked:bg-gray-900"></div>
+                    <span className="ml-3 text-sm font-bold text-gray-900 dark:text-gray-300">Dark</span>
+                </label>
             </div>
         </div>
     );
